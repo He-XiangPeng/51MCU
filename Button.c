dbede28d8f6e3ff9dda22cb0debd4045d97f6b0a
He-XiangPeng <hxpeng1989@hotamil.com>
2019-03-13 21:32:43 +0800

    add Button.c

---
+++ Button.c
@@ -0,0 +1,37 @@
+/**
+* 独立按键，控制LED灯状态的翻转
+*
+*/
+#include <reg52.h>
+typedef unsigned char uchar;
+typedef unsigned int uint;
+
+sbit LED_1 = P2^0;
+sbit K1 = P3^1;
+
+void delay(uint i)
+{
+	while(i--); // 当i为0时循环停止
+}
+
+void keypros(void)
+{
+	if(0 == K1) // 按键是否按下
+	{
+		delay(1000); // 消抖
+		if(0 == K1)  // 再次判断按键是否按下
+			LED_1 = ~LED_1;
+		while(!K1); // 
+	}
+}
+
+void main(void)
+{
+
+// 单个LED闪烁
+	while(1)
+	{
+		keypros();
+	}
+
+}
