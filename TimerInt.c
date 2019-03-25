7be643aed5f464162379f614287540bb6b3672e7
He-XiangPeng <hxpeng1989@hotamil.com>
2019-03-25 21:52:21 +0800

    add TimerInt.c

---
+++ TimerInt.c
@@ -0,0 +1,39 @@
+/**
+*	定时器中断0
+*
+*/
+#include <reg52.h>
+typedef unsigned char uchar;
+typedef unsigned int uint;
+sbit LED = P2^2;
+
+// 定时中断0
+void Timer0Init()
+{
+	TMOD |= 0x01;
+	TH0 = 0xfc;
+	TL0 = 0x18;
+	ET0 = 1;
+	EA  = 1;
+	TR0 = 1;
+}
+
+void main()
+{
+	Timer0Init();
+	while(1);
+}
+
+// 定时1s
+void Timer0() interrupt 1
+{
+	static uint i;
+	TH0 = 0xfc;
+	TL0 = 0x18;
+	i++;
+	if(1000 == i) // 1s
+	{
+		i = 0;
+		LED = ~LED;
+	}	
+}
